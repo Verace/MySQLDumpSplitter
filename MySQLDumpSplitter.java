64075b01933ff15203b3aced94d2a96ff1def646
Pól Ua Laoínecháin <linehanp@tcd.ie>
2014-02-07 20:46:25 +0000

    Create MySQLDumpSplitter.java

---
+++ MySQLDumpSplitter.java
@@ -0,0 +1,17 @@
+package com.relvar.mysqldumpsplitter;
+
+
+public class MySQLDumpSplitter
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    
+    FrMySQLDumpSplitter frMDS = new FrMySQLDumpSplitter();
+    frMDS.setVisible(true);
+  }
+  
+}
